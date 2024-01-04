a8f1c4053c4c8e134b5d0c7a8d97324f8f922b53
mariga <mariga.bn@gmail.com>
2024-01-04 20:54:40 +0300

    For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const myArray = [];
+  
+    for (const value of array) {
+      myArray.push(appendString + value);
+    }
+  
+    return myArray;
+  }
